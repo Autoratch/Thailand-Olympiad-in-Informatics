c9de3a065ad192a8f935a33fc38a22792a973d69
Auto Krittitee Ratchanan <autorockza@gmail.com>
2018-03-05 10:59:37 +0700

    Update toi02_segment.cpp

--- toi02_segment.cpp
+++ toi02_segment.cpp
@@ -4,8 +4,44 @@ using namespace std;
 #define MOD 1e9 + 7
 
 int a,b;
-vector<vector<char> > > s1,s2;
+vector<vector<vector<char> > > s1,s2;
 vector<int> x1,x2;
+
+int val(vector<vector<char> > x)
+{
+    if(x[0][1]==' ')
+    {
+        if(x[1][0]==' ') return 1;
+        else return 4;
+    }
+    if(x[2][1]==' ') return 7;
+    if(x[1][0]==' ' and x[2][0]==' ') return 3;
+    if(x[1][0]==' ') return 2;
+    if(x[1][1]==' ') return 0;
+    if(x[2][0]==' ')
+    {
+        if(x[1][2]!='|') return 5;
+        else return 9;
+    }
+    if(x[1][2]!='|') return 6;
+    return 8;
+}
+
+void pl()
+{
+    reverse(x1.begin(),x1.end());
+    reverse(x2.begin(),x2.end());
+    for(int i = a;i < max(a,b);i++) x1.push_back(0);
+    for(int i = b;i < max(a,b);i++) x2.push_back(0);
+    vector<int> ans;
+    for(int i = 0;i < max(a,b);i++)
+    {
+        if(x1[i]+x2[i]>9){ ans.push_back((x1[i]+x2[i])%10); if(i==max(a,b)-1) ans.push_back(1); else x1[i+1]++; }
+        else ans.push_back(x1[i]+x2[i]);
+    }
+    reverse(ans.begin(),ans.end());
+    for(int i = 0;i < ans.size();i++) cout << ans[i];
+}
 
 int main()
 {
@@ -28,14 +64,26 @@ int main()
             {
                 char c;
                 c = getchar();
-                s[j][i][k] = c;
+                s1[j][i][k] = c;
             }
+            getchar();
         }
-        getchar();
     }
-
+    for(int i = 0;i < 3;i++)
+    {
+        for(int j = 0;j < b;j++)
+        {
+            s2[j][i].resize(3);
+            for(int k = 0;k < 3;k++)
+            {
+                char c;
+                c = getchar();
+                s2[j][i][k] = c;
+            }
+            getchar();
+        }
+    }
     for(int i = 0;i < a;i++) x1[i] = val(s1[i]);
     for(int i = 0;i < b;i++) x2[i] = val(s2[i]);
-
     pl();
 }
