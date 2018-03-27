56870c5ed3aa0fe886e977f3ec2ba6b23f015ab5
Auto Krittitee Ratchanan <autorockza@gmail.com>
2018-03-27 15:18:22 +0700

    Update toi10_climbingmonkey.cpp

--- toi10_climbingmonkey.cpp
+++ toi10_climbingmonkey.cpp
@@ -7,82 +7,84 @@ int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    int m,n,k,s,mxa = 0;
+    int m,n,k,mx = 0,s;
+    bool used = false;
 
     cin >> m >> n >> k;
 
-    vector<int> a(n);
-    vector<vector<pair<int,int> > > adj(n);
+    vector<int> a(n),idh(m+1);
+    vector<bool> visit(m+1,false);
+    vector<pair<int,int> > b(k);
 
-    for(int i = 0;i < n;i++){ cin >> a[i]; mxa = max(mxa,a[i]); }
+    for(int i = 0;i < n;i++){ cin >> a[i]; mx = max(mx,a[i]); }
+
     for(int i = 0;i < k;i++)
     {
-        int idx,h;
-        cin >> idx >> h;
-        adj[idx-1].push_back({h,1});
-        adj[idx].push_back({h,-1});
+        int h,x;
+        cin >> x >> h;
+        b[i] = {h,x-1};
     }
-
-    for(int i = 0;i < n;i++) sort(adj[i].begin(),adj[i].end());
 
     cin >> s; s--;
+    sort(b.begin(),b.end());
+    reverse(b.begin(),b.end());
 
-    queue<pair<pair<int,int>,bool> > q;
-
-    int mx = 0,us;
-
-    q.push({{s,0},0});
-
-    while(!q.empty())
+    for(int i = 0;i < k;i++)
     {
-        int p = q.front().first.first,h = q.front().first.second;
-        bool used = q.front().second;
-        q.pop();
-        auto it = lower_bound(adj[p].begin(),adj[p].end(),make_pair(h,2));
-        if(it==adj[p].end())
-        {
-            if(a[p]>mx){ mx = a[p]; us = used; }
-            if(used) continue;
-        }
-        if(!used)
-        {
-            if(p>0)
-            {
-                auto it2 = lower_bound(adj[p-1].begin(),adj[p-1].end(),make_pair(h,2));
-                if(it!=adj[p].end()) for(;it2-1 != adj[p-1].end() and it2->first<it->first;it2++)
-                {
-                    q.push({{p-1,it2->first-1},true});
-                    auto it3 = it2; it3++;
-                    if(it3==adj[p-1].end()) q.push({{p-1,it2->first},true});
-                }
-                else for(;it2 != adj[p-1].end();it2++)
-                {
-                    q.push({{p-1,it2->first-1},true});
-                    auto it3 = it2; it3++;
-                    if(it3==adj[p-1].end()) q.push({{p-1,it2->first},true});
-                }
-            }
-            if(p+1<n)
-            {
-                auto it2 = lower_bound(adj[p+1].begin(),adj[p+1].end(),make_pair(h,2));
-                if(it!=adj[p].end()) for(;it2-1 != adj[p+1].end() and it2->first<it->first;it2++)
-                {
-                    q.push({{p+1,it2->first-1},true});
-                    auto it3 = it2; it3++;
-                    if(it3==adj[p+1].end()) q.push({{p+1,it2->first},true});
-                }
-                else for(;it2 != adj[p+1].end();it2++)
-                {
-                    q.push({{p+1,it2->first-1},true});
-                    auto it3 = it2; it3++;
-                    if(it3==adj[p+1].end()) q.push({{p+1,it2->first},true});
-                }
-            }
-        }
-        if(it!=adj[p].end()) q.push({{p+(it->second),it->first},used});
+        int x = b[i].second,y = b[i].second+1;
+        int tmp = a[x];
+        a[x] = a[y];
+        a[y] = tmp;
     }
 
-    cout << mx;
-    if(us) cout << "\nUSE";
-    else cout << "\nNO";
+    if(a[s]==mx){ cout << mx << endl << "NO"; return 0; }
+
+    reverse(b.begin(),b.end());
+
+    for(int i = 0;i < k;i++) if(!visit[b[i].first]){ idh[b[i].first] =  i; visit[idh[b[i].first]] = true; }
+
+    int h = 0,idx = 0,p = s;
+    mx = a[s];
+
+    while(true)
+    {
+        int px = p;
+        if(b[idx].second==p)
+        {
+            p++;
+            h = b[idx].first;
+            idx++;
+        }
+        else if(b[idx].second==p-1)
+        {
+            p--;
+            h = b[idx].first;
+            idx++;
+        }
+        if(p>0) if(a[p-1]>mx){ mx = a[p-1]; used = true; }
+        if(p<n-1) if(a[p+1]>mx){ mx = a[p+1]; used = true; }
+        if(idx==b.size()) break;
+        int tmp = a[b[idx].second];
+        a[b[idx].second] = a[b[idx].second+1];
+        a[b[idx].second+1] = tmp;
+        if(p>0) if(a[p-1]>mx){ mx = a[p-1]; used = true; }
+        if(p<n-1) if(a[p+1]>mx){ mx = a[p+1]; used = true; }
+        int tmpp = idh[h];
+
+        while(true)
+        {
+            if(tmpp==b.size()) break;
+            if(b[tmpp].second==p-2) if(a[p-2]>mx){ mx = a[p-2]; used = true; }
+            if(b[tmpp].second==p+1) if(a[p+2]>mx){ mx = a[p+2]; used = true; }
+            if(b[tmpp].second==p or b[tmpp].second==p-1) break;
+            tmpp++;
+        }
+
+        idx++;
+        if(idx==b.size()) break;
+    }
+
+    cout << mx << endl;
+    if(used) cout << "USE";
+    else cout << "NO";
 }
