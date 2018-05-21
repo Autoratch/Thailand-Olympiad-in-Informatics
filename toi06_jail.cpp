dd594ad7f97e3f8d84052d2db0cb93cb4b5a55e2
Auto Krittitee Ratchanan <autorockza@gmail.com>
2018-05-21 17:29:29 +0700

    Update toi06_jail.cpp

--- toi06_jail.cpp
+++ toi06_jail.cpp
@@ -3,29 +3,23 @@ using namespace std;
 #define endl '\n'
 #define MOD 1e9 + 7
 
+int n,k,fw[1000001],bw[1000001],cur;
+
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    int n,k;
-
     cin >> n >> k;
 
-    vector<int> f(n+1),b(n+1);
+    for(int i = 0;i < n;i++) fw[i] = i+1;
+    for(int i = 2;i <= n;i++) bw[i] = i-1;
+    fw[n] = 1; bw[1] = n;
 
-    for(int i = 1;i < n;i++) f[i] = i+1;
-    for(int i = 2;i <= n;i++) b[i] = i-1;
-    f[n] = 1;
-    b[1] = n;
-
-    int idx = 1;
-
-    for(int cn = 0;cn < n;cn++)
+    for(int i = 0;i < n;i++)
     {
-        for(int i = 1;i < k;i++) idx = f[idx];
-        cout << idx << ' ';
-        b[f[idx]] = b[idx];
-        f[b[idx]] = f[idx];
-        idx = f[idx];
+        for(int j = 0;j < k;j++) cur = fw[cur];
+        cout << cur << ' ';
+        fw[bw[cur]] = fw[cur];
+        bw[fw[cur]] = bw[cur];
     }
 }
